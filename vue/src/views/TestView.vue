84b541fed6f8a14df9a3be967168f3604b984e7a
Y_RyuZU <ryuzu1006@gmail.com>
2022-09-16 21:22:25 +0900

    initial commit

---
+++ vue/src/views/TestView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p v-for="n of count" :key="n">{{text}}</p>
+    </div>
+</template>
+
+<script lang="ts">
+    import { defineComponent,ref } from "vue"
+
+    export default defineComponent({
+        setup(props, ctx) {
+            const text = ref("HEY!!!")
+            const count = ref(5)
+
+            setTimeout(() => {
+                text.value = "AAAAAA"
+                count.value = 20
+            }, 3 * 1000)
+
+            return {
+                text,
+                count
+            }
+        },
+    })
+</script>
+
+<style scoped>
+
+</style>
