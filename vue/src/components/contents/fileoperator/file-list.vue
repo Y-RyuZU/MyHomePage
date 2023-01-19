250989fc7d5b3fa37b43e50c285ec2654454bbbf
Y-RyuZU <ryuzu1006@gmail.com>
2023-01-19 19:49:35 +0900

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 87cb041b93ecdd12ce9f6e23efcbcc162c867777, reversing
    changes made to 78035f978388a70464caccf8dc4e12ca97517673.

--- vue/src/components/contents/fileoperator/file-list.vue
+++ vue/src/components/contents/fileoperator/file-list.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="display: flex; align-items: center">
+    <div style="display: flex; align-items: center" @drop.prevent="onDrop">
         <file-list-tree/>
         <el-table
             :data="tableData"
@@ -49,6 +49,7 @@ import {Folder} from '@element-plus/icons-vue'
 import FileListHeader from "@/components/contents/fileoperator/file-list-header.vue";
 import FileListTree from "@/components/contents/fileoperator/file-list-tree.vue";
 import { defineStore } from "pinia";
+import axios from "axios";
 
 interface File {
     name: string
@@ -69,6 +70,27 @@ const getType = (name: string) => {
 const handleCurrentChange = (val: File | undefined) => {
     selecting.value = val
 }
+
+const onDrop = (e: DragEvent) => {
+    e.preventDefault()
+    e.stopPropagation()
+    const file = e.dataTransfer?.files
+    if (!file) return
+    const files = [...file]
+    files.forEach(file => {
+        const url = `http://localhost:8080/api/files/upload`
+        const path = 'debug/path1'
+        let form = new FormData()
+        form.append('file', file)
+        form.append('path', path)
+        axios.post(url, form).then(response => {
+            console.log(response.data)
+        }).catch(error => {
+            console.log(error)
+        })
+    })
+}
+
 const tableData = [
     {
         name: 'None1',
