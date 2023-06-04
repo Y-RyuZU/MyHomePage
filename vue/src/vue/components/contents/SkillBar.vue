f203cd32eca49c865e84446187d922d265142eba
Y_RyuZU <ryuzu1006@gmail.com>
2023-06-04 23:22:41 +0900

    Changes

---
+++ vue/src/vue/components/contents/SkillBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container>
+        <v-card>
+            <v-card-title>
+                Sales Statistics
+            </v-card-title>
+            <v-card-text>
+                <div v-for="(item, index) in salesData" :key="index">
+                    <span>{{ item.name }}</span>
+                    <v-progress-linear
+                            :model-value="item.value"
+                            height="20"
+                            color="blue-grey"
+                            striped
+                    ></v-progress-linear>
+                </div>
+            </v-card-text>
+        </v-card>
+    </v-container>
+</template>
+
+<script setup lang="ts">
+const salesData = [
+    {name: "Q1", value: 10},
+    {name: "Q2", value: 15},
+]
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- vue/src/vue/pages/Portfolio.vue
+++ vue/src/vue/pages/Portfolio.vue
@@ -1,7 +1,7 @@
 <template>
     <v-parallax
-        :src="img"
-        height="1000"
+            :src="img"
+            height="1000"
     >
         <div class="d-flex flex-column fill-height justify-center align-center text-white">
             <h1 class="text-h4 font-weight-thin mb-4">
@@ -12,12 +12,36 @@
             </h4>
         </div>
     </v-parallax>
-    <p>peropero</p>
+
+    <v-container>
+        <v-row>
+            <v-col
+                    v-for="n in 12"
+                    :key="n"
+                    cols="4"
+            >
+                <SkillCards/>
+            </v-col>
+        </v-row>
+    </v-container>
+
+    <v-container>
+        <v-row>
+            <v-col
+                    v-for="n in 12"
+                    :key="n"
+                    cols="4"
+            >
+                <SkillBar/>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script setup lang="ts">
 import img from "@/assets/images/background3.jpg";
-</script>
+import SkillCards from "@/vue/components/contents/SkillCards.vue";
+import SkillBar from "@/vue/components/contents/SkillBar.vue";</script>
 
 <style scoped lang="scss">
 
